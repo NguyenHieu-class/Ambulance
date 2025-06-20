ba5cff10d55af3746906be97e7cf209dc19cff43
NguyenHieu-class <121001445+NguyenHieu-class@users.noreply.github.com>
2025-06-20 12:19:36 +0700

    Add Trip, Hospital, and PaymentBill entities

--- src/main/java/com/project/Ambulance/model/Hospital.java
+++ src/main/java/com/project/Ambulance/model/Hospital.java
@@ -1,4 +1,32 @@
 package com.project.Ambulance.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "hospitals")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Hospital {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idHospital;
+
+    @Column(columnDefinition = "nvarchar(200) not null")
+    private String nameHospital;
+
+    @Column(columnDefinition = "nvarchar(200)")
+    private String streetAddress;
+
+    @Column(columnDefinition = "nvarchar(100)")
+    private String city;
+
+    @Column(columnDefinition = "nvarchar(100)")
+    private String phone;
+
+    @Column(columnDefinition = "nvarchar(100)")
+    private String email;
 }
